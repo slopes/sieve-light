e0a482b059e6651c7be890323b19cea1c77b550b
slopes <stephane.lopes.bogalho@gmail.com>
2018-12-16 15:51:25 +0100

    Add simple red light

--- light.py
+++ light.py
@@ -1,9 +1,14 @@
 import logging
+import unicornhat as uh
 
 def red():
     logging.info('red alert') 
+    uh.set_pixel(0, 0, 255, 0, 0)
+    uh.show()
     return True
 
 def clear():
     logging.info('No alert') 
+    uh.clear()
+    uh.show()
     return True
