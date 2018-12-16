cd26c26b68134bc29b811c4490381793e1fe01d4
slopes <stephane.lopes.bogalho@gmail.com>
2018-12-16 15:55:16 +0100

    Fix server

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import render_template
 import connexion
 import logging
+import unicornhat as uh
+
 
 
 # create the application instance
@@ -12,5 +14,7 @@ app.add_api("swagger.yml")
 
 if __name__ == "__main__":
     logging.info('Server stating') 
+    uh.set_layout(uh.PHAT)
+    uh.brightness(0.5)
     app.run(debug=True)
 
