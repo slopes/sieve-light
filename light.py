1a89aaa4bb7b9327b1b89d1a62ab6e63437b663a
slopes <stephane.lopes.bogalho@gmail.com>
2018-12-16 16:20:22 +0100

    Fix light api

--- light.py
+++ light.py
@@ -1,17 +1,20 @@
 import logging
 import unicornhat as uh
 
-uh.set_layout(uh.PHAT)
-uh.brightness(0.5)
+
 
 def red():
     logging.info('red alert') 
+    uh.set_layout(uh.PHAT)
+    uh.brightness(0.5)
     uh.set_pixel(0, 0, 255, 0, 0)
     uh.show()
     return True
 
 def clear():
     logging.info('No alert') 
+    uh.set_layout(uh.PHAT)
+    uh.brightness(0.5)
     uh.clear()
     uh.show()
     return True
